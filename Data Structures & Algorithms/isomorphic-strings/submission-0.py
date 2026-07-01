4c6afc6e4d071dc0275dcb7a2a954198926b8d78
Bharath Royal <bharathkumarchavur187i@gmail.com>
2026-07-01 23:09:26 +0530

    Add: isomorphic-strings - submission-0

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        mapST = {}
+        mapTS = {}
+
+        for c1, c2 in zip(s, t):
+            if c1 in mapST:
+                if mapST[c1] != c2:
+                    return False
+            else:
+                mapST[c1] = c2
+
+            if c2 in mapTS:
+                if mapTS[c2] != c1:
+                    return False
+            else:
+                mapTS[c2] = c1
+
+        return True
